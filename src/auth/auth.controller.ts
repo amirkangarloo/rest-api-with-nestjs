982c30aacb21d6f2913635fcef9cfe6bacecc641
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-06-23 13:27:47 +0430

    Implement validation and dto for auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,19 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService) { }
 
     @Post('singup')
-    singup(@Body() dto: any){
+    singup(@Body() dto: AuthDto) {
         return this.authService.singup()
     }
-    
+
     @Post('singin')
-    singin(){
+    singin() {
         return this.authService.singin()
     }
-    
+
 }
